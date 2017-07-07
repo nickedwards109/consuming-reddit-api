1e05dfde306c8643ee4690264548e9d649643af8
nicholasedwards <nicholas.lee.edwards@gmail.com>
2017-07-07 15:02:40 -0600

    modify front page spec for login/logout
    if there is no current user, the front page should have a login link. if there is a current user, the front page should have a logout link.

--- spec/features/user_views_front_page_spec.rb
+++ spec/features/user_views_front_page_spec.rb
@@ -1,20 +1,21 @@
 require 'rails_helper'
 
 RSpec.feature "Front Page" do
-  before(:each) do
+  it "displays the user's username and karma" do
+    visit "/"
+    expect(page).to have_content "Login"
+
     user_mock = User.create(access_token: "mocktoken1a81jv0ck4")
     user_mock.username = "RedditUserName"
     user_mock.karma = 16
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_mock)
-  end
 
-  it "displays the user's username and karma" do
     visit "/"
     expect(page).to have_selector(".username")
     expect(page).to have_content("RedditUserName")
     expect(page).to have_selector(".karma")
     expect(page).to have_content("16")
     expect(page).to have_selector(".inbox")
-    expect(page).to have_selector(".logout")
+    expect(page).to have_content("Logout")
   end
 end
