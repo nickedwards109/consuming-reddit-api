7c3579e0b64170303e2ec0e85fe87bbca5d1a6bc
nicholasedwards <nicholas.lee.edwards@gmail.com>
2017-07-08 16:24:13 -0600

    add specs for subscribed subreddits display on front page

--- spec/features/user_views_front_page_spec.rb
+++ spec/features/user_views_front_page_spec.rb
@@ -1,16 +1,22 @@
 require 'rails_helper'
 
 RSpec.feature "Front Page" do
-  it "displays the user's username and karma" do
+  it "displays the user's username, karma, and subscribed subreddits" do
     visit "/"
     expect(page).to have_content "Login"
 
     user_mock = User.create(access_token: "mocktoken1a81jv0ck4")
     user_mock.username = "RedditUserName"
     user_mock.karma = 16
+    subreddit = Subreddit.new(title: "programming")
+    user_mock.subreddits << subreddit
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_mock)
 
     visit "/"
+    expect(page).to have_selector(".subscribed-subreddits")
+    within(".subscribed-subreddits") do
+      expect(page).to have_content("programming")
+    end
     expect(page).to have_selector(".username")
     expect(page).to have_content("RedditUserName")
     expect(page).to have_selector(".karma")
